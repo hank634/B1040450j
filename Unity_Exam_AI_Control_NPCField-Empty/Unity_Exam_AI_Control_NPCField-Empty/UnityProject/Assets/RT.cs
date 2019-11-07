65bd798443ceddcf68ffea128995889857881f06
hank634 <55218499+hank634@users.noreply.github.com>
2019-11-07 12:02:26 +0800

    12

---
+++ Unity_Exam_AI_Control_NPCField-Empty/Unity_Exam_AI_Control_NPCField-Empty/UnityProject/Assets/RT.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RT : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) { transform.Translate(0, 0, 1f); }
+
+        if (Input.GetKeyDown(KeyCode.S)) { transform.Translate(0, 0, -1f); }
+
+      
+
+    }
+}
---
+++ Unity_Exam_AI_Control_NPCField-Empty/Unity_Exam_AI_Control_NPCField-Empty/UnityProject/Assets/ball.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    [Header("對話")]
+    public string 對話 = "給我汽油";
+    public float 對話速度 = 1.5f;
+    [Header("任務")]
+    public bool 是否完成 = false;
+    public int 數量 = 0;
+    public int 需求 = 5;
+}
